dc701a21e45c834858d5c006579050f91f751451
David Viloria <david.viloria@citi.com.mx>
2024-04-27 21:28:45 -0500

    Initial Commit

---
+++ BankAppDemo/BankAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppDemoApp.swift
+//  BankAppDemo
+//
+//  Created by David Viloria Ortega on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BankAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
