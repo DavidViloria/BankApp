024afc2063f33202d0a07b3c933fea840a06adc1
David Viloria <david.viloria@citi.com.mx>
2024-04-27 23:22:42 -0500

    Creación de vista inicial para el menú lateral.

--- BankAppDemo/HomeView/HomeView.swift
+++ BankAppDemo/HomeView/HomeView.swift
@@ -10,35 +10,83 @@ import Combine
 
 struct HomeView: View {
     @Binding var isPresented: Bool
-    
+    @State var showMenu = false
+
     var body: some View {
-        VStack {
-            Text("Bienvenido Ricardo")
-                .font(.headline)
-            Text("Tu saldo es")
-                .font(.headline)
-            
-            Text("$ 1,203.56")
-                .font(.title)
-            
-            HStack {
-                Button(action: {
-                    // Acción para transferir
-                }) {
-                    Text("Transferir")
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("Bienvenido Ricardo")
+                        .font(.headline)
+                    Text("Tu saldo es")
+                        .font(.headline)
+                    Text("$ 1,203.56")
+                        .font(.title)
+                    Spacer()
                 }
-                
-                Spacer()
-                
-                Button(action: {
-                    // Acción para mi cuenta
-                }) {
-                    Text("Mi cuenta")
+                .padding()
+                .blur(radius: showMenu ? 20 : 0)
+                .animation(.default)
+
+                if showMenu {
+                    MenuView(showMenu: $showMenu)
                 }
             }
+            .navigationBarItems(leading: (
+                Button(action: {
+                    withAnimation {
+                        self.showMenu.toggle()
+                    }
+                }) {
+                    Image(systemName: "line.horizontal.3")
+                        .imageScale(.large)
+                }
+            ))
         }
-        .padding()
     }
 }
+
+struct MenuView: View {
+    @Binding var showMenu: Bool
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Button(action: {
+                // Acción para perfil
+            }) {
+                Text("Perfil")
+            }
+            Button(action: {
+                // Acción para sección de ayuda
+            }) {
+                Text("Sección de Ayuda")
+            }
+            Button(action: {
+                // Acción para ver perfil
+            }) {
+                Text("Ver Perfil")
+            }
+            Button(action: {
+                // Acción para pagos de servicio
+            }) {
+                Text("Pagos de Servicio")
+            }
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.white)
+        .offset(x: showMenu ? 0 : -UIScreen.main.bounds.width)
+        .animation(.default)
+        .onTapGesture {
+            withAnimation {
+                self.showMenu = false
+            }
+        }
+    }
+}
+
+
+
 
 
